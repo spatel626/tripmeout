5ea973c60de5e7645d21b5e134357b344949a8d1
Samit Patel <spatel85@gmail.com>
2021-12-26 23:39:49 -0800

    Updates

--- _next/static/Vvtefyx-hsSATwp7AEvWQ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/061e6b60-28bd444bb64e34ae.js","static/chunks/169-e386b65b31454e88.js","static/chunks/pages/index-7cef32b535631ba6.js"],"/_error":["static/chunks/pages/_error-a3f18418a2205cb8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
